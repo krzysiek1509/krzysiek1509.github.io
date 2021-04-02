330eba92f189d8370c324d9072c3c16be65e1278
Krzysztof Wańczura <krzysiek1509@poczta.onet.pl>
2021-04-02 12:58:29 +0200

    js

---
+++ Lekcja 9/script.js
@@ -0,0 +1,51 @@
+const views = [
+    {
+        src : "1.jpg",
+        title : "Zachód pod palmami",
+    },
+    {
+        src : "2.jpg",
+        title : "Widok z lotu ptaka",
+    },
+    {
+        src : "3.jpg",
+        title : "Mordor wersja Polska",
+    }
+];
+let liczSlajd = 0; //zmienna którą wykorzystamy do zmiany obrazków
+
+function showslide (numer) {
+const titleField = document.querySelector(".tymczasowy");
+titleField.innerHTML = views[numer].title;
+
+const obrazek = document.querySelector(".content");
+obrazek.innerHTML = "";    //Czyści poprzedni obrazek, ważne !!
+const zdjecie = document.createElement("img");
+zdjecie.src = "img/" + views[numer].src;
+obrazek.append(zdjecie);
+};
+
+showslide(liczSlajd);
+//Kliknięcie w lewą strzałkę
+const leftArr = document.querySelector(".left");
+leftArr.onclick = () => {zmianaSlajdu("lewa")};
+
+//Kliknięcie w prawą strzałkę
+const rightArr = document.querySelector(".right");
+rightArr.onclick = () => {zmianaSlajdu("prawa")};
+
+//funkcja do zmiany obrazku
+function zmianaSlajdu ( kierunek){
+    if( kierunek == "lewa"){
+        liczSlajd-- ; 
+        if(liczSlajd<0) { liczSlajd = views.length-1;}}
+    if(kierunek == "prawa"){
+            liczSlajd++; 
+        if(liczSlajd > views.length-1) { liczSlajd = 0; }
+            }
+        
+     console.log(liczSlajd);
+     showslide(liczSlajd);
+        };
+
+
