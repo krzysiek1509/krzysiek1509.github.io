905006a24b8d0f33715d3d8a82964ebd58ef69c3
Krzysztof Wańczura <krzysiek1509@poczta.onet.pl>
2021-04-06 15:30:18 +0200

    dokończyć tabelkę

---
+++ inc/js/scriptp.js
@@ -0,0 +1,27 @@
+const tablicaObraz = [
+    {
+        src : "1",
+        ht : "50",
+        cs : "60",
+        javscr : "0",
+},
+{
+    src : "2",
+    ht : "60",
+    cs : "50",
+    javscr : "30",
+},
+{
+    src : "3",
+        ht : "40",
+        cs : "50",
+        javscr : "50",
+}
+]
+
+let index = 0;
+
+function zmianaKolumny (numer){
+    const Kolumna1 = document.querySelector("#k1"); 
+    
+}
