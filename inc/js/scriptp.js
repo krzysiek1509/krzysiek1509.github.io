8faecf61ba87a9fe557bb58928542bcf5f38d80a
Krzysztof Wańczura <krzysiek1509@poczta.onet.pl>
2021-04-10 15:24:14 +0200

    dokończenie wykresu

--- inc/js/scriptp.js
+++ inc/js/scriptp.js
@@ -2,20 +2,20 @@ const tablicaObraz = [
     {
         src : "1",
         ht : "50",
-        cs : "60",
+        cs : "50",
         javscr : "0",
 },
 {
     src : "2",
     ht : "60",
     cs : "50",
-    javscr : "30",
+    javscr : "50",
 },
 {
     src : "3",
         ht : "40",
-        cs : "50",
-        javscr : "50",
+        cs : "30",
+        javscr : "40",
 }
 ]
 
@@ -23,5 +23,31 @@ let index = 0;
 
 function zmianaKolumny (numer){
     const Kolumna1 = document.querySelector("#k1"); 
-    
+    Kolumna1.style.height = tablicaObraz[numer].ht + "px";
+    const Kolumna2 = document.querySelector("#k2"); 
+    Kolumna2.style.height = tablicaObraz[numer].cs + "px";
+    const Kolumna3 = document.querySelector("#k3"); 
+    Kolumna3.style.height = tablicaObraz[numer].javscr + "px";
 }
+
+const obraz1 = document.querySelector("#projekt1");
+const obraz2 = document.querySelector("#projekt2");
+const obraz3 = document.querySelector("#projekt3");
+
+
+obraz1.onmouseover = () => {zmianaKolumny(0)}; 
+obraz2.onmouseover = () => {zmianaKolumny(1)}; 
+obraz3.onmouseover = () => {zmianaKolumny(2)}; 
+/*
+
+obraz1.onmouseout = () => { brakWykresu()};
+
+function brakWykresu(){
+const Wykres = document.querySelector("aside");
+Wykres.style.zIndex="-1";
+}
+function wiekszyObrazek(rozmiar){
+    ["Obraz" + tablicaObraz[rozmiar].src].style.height = 30vh;
+    ["Obraz" + tablicaObraz[rozmiar].src].style.width = 30vw;
+    }
+    */
