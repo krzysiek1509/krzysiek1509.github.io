716e9e16e01b87897c598688b60540088c9aaed9
Krzysztof Wańczura <krzysiek1509@poczta.onet.pl>
2021-04-10 17:53:25 +0200

    umiejętności

---
+++ inc/js/scripts.js
@@ -0,0 +1,7 @@
+const obrazek1 = document.querySelector(".ikona1");
+obrazek1.onclick = () => {kolumna1()}
+
+funkcja kolumna1(){
+    const kl1 = document.querySelector("#s1");
+    kl1.style.zIndex = "3";
+}
