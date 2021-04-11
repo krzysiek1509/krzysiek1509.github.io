88b3fe6a8353779b43a0b8b7e5e4502c495a56e0
Krzysztof Wańczura <krzysiek1509@poczta.onet.pl>
2021-04-11 11:00:50 +0200

    dodanie js

--- inc/js/scripts.js
+++ inc/js/scripts.js
@@ -1,7 +1,21 @@
 const obrazek1 = document.querySelector(".ikona1");
-obrazek1.onclick = () => {kolumna1()}
+obrazek1.onclick = () => {kolumna1(1)};
 
-funkcja kolumna1(){
-    const kl1 = document.querySelector("#s1");
-    kl1.style.zIndex = "3";
-}
+function kolumna1 (numer){
+    const kl1 = document.querySelector(".s1");
+    kl1.style.opacity = numer;
+};
+const obrazek2 = document.querySelector(".ikona2");
+obrazek2.onclick = () => {kolumna2(1)};
+
+function kolumna2 (numer){
+    const kl2 = document.querySelector(".s2");
+    kl2.style.opacity = numer;
+};
+const obrazek3 = document.querySelector(".ikona3");
+obrazek3.onclick = () => {kolumna3(1)};
+
+function kolumna3 (numer){
+    const kl3 = document.querySelector(".s3");
+    kl3.style.opacity = numer;
+};
